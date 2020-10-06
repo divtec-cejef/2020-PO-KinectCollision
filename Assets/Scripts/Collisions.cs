f01d2eba023b977be443458dabaf3ce05c99ddd9
Louis Bovay <54527880+EctoBoggan@users.noreply.github.com>
2020-10-06 16:15:20 +0200

    Ajout du projet Unity

---
+++ Assets/Scripts/Collisions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Gère les collisions entre le personnage et les objets présents
+public class Collisions : MonoBehaviour   
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider cube)
+    {
+        GameObject gameObjectCube = cube.gameObject;
+        gameObjectCube.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    private void OnTriggerExit(Collider cube)
+    {
+        GameObject gameObjectCube = cube.gameObject;
+        gameObjectCube.GetComponent<Renderer>().material.color = Color.green;   
+    }
+}
